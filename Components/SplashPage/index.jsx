fde3ca02c20280fc97683f0dcad5e80a5919f7f3
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-06-07 11:03:51 -0700

    using react-native-paper

--- Components/SplashPage/index.jsx
+++ Components/SplashPage/index.jsx
@@ -1,46 +1,22 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
 
-export default function SplashPage() {
-	return (
-		<React.Fragment>
-			<Card>
-				<CardContent>
-					<Typography sx={{ fontSize: 14 }} color="" gutterBottom>
-						App Text
-					</Typography>
-				</CardContent>
-				<CardActions>
-					<Stack spacing={2} direction="row">
-						<Button variant="contained" color="">Login</Button>
-						<Button variant="outlined" color="">Sign Up</Button>
-					</Stack>
-					<Stack align="center">
-						<Button color="">Continue as a Guest</Button>
-					</Stack>
-					<Stack spacing={6} direction="row">
-						<IconButton aria-label="dark">
-							<MoonIcon />
-						</IconButton>
-						<IconButton aria-label="light">
-							<SunIcon />
-						</IconButton>
-					</Stack>
-				</CardActions>
-			</Card>
-		</React.Fragment>
-	)
-};
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
-// export default function OutlinedCard() {
-//   return (
-//     <Box sx={{ minWidth: 275 }}>
-//       <Card variant="outlined">{card}</Card>
-//     </Box>
-//   );
-// }
+const SplashPage = () => (
+  <Card>
+    <Card.Title title="Forum" subtitle="Questions/Answers" left={LeftContent} />
+    <Card.Content>
+      <Text variant="titleLarge">Forum</Text>
+      <Text variant="bodyMedium">Questions/Answers</Text>
+    </Card.Content>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    <Card.Actions>
+      <Button>Log In</Button>
+      <Button>Sign Up</Button>
+			<Button>Continue As Guest</Button>
+    </Card.Actions>
+  </Card>
+);
+
+export default SplashPage;
