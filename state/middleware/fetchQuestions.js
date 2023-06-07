ab1fbdb04feef0d63cca18194b48f9c0e2677f96
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-06-07 15:33:52 -0700

    fetching questions and state for questions working, added a little bit of modal stuff

--- state/middleware/fetchQuestions.js
+++ state/middleware/fetchQuestions.js
@@ -1,10 +1,11 @@
 export const fetchQuestions = () => async (dispatch) => {
-  let response = await fetch('https://ourRenderSite.com/QUESTIONS');
+  let response = await fetch('https://bfff.onrender.com/forum/questions');
   let data = await response.json();
   let questions = data.results;
+  console.log(data);
 
   dispatch({
     type: 'FETCH_QUESTIONS',
-    payload: questions,
+    payload: data,
   });
 }
